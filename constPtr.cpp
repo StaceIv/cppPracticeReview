4bf0457f900142711cc8b9bbc992987bac054aa3
StaceIv <stacey.ivanovic007@gmail.com>
2017-04-28 22:55:24 -0700

    Removed a file

--- constPtr.cpp
+++ constPtr.cpp
@@ -16,16 +16,22 @@ int main(int argc, char *argv[]){
      //If we declare ptr as:
      //const char *ptr = &a
      //then it cannot modify what it points to.
-     
+    
+
+     //ptr = &a assigns the address of a to ptr. The thing 
+     //contained at address can change!!! <- this is important. 
      cout << "a " << a << endl;
      cout << "&a " << &a << endl;
      cout << "b " << b << endl;
      cout << "*ptr " << *ptr << endl;
      cout << "&ptr " << &ptr << endl;
 
-     //We can change what ptr points to.
+     //We can change what ptr points to. So the pointer is still
+     //pointing to address &a, however now 'b' is at that address.
      *ptr = 'b';     
 
+     //Now when we print a, var a is now 'b'. But the address is 
+     //still the same! Cool.
      cout << "a " << a << endl;
      cout << "&a " << &a << endl;
      cout << "b " << b << endl;
