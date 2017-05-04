5697941589f1452a284aeda3fc171888496508d2
StaceIv <stacey.ivanovic007@gmail.com>
2017-05-03 19:10:49 -0700

    Constructor not constructing

--- useCircle.cpp
+++ useCircle.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
-#include "Circle.hpp"
+#include <string>
+//#include "Circle.hpp"
 //#include "Circle.cpp"
 
 using namespace std;
 
+class Circle{
+	//private means it can only be accessed by the member 
+	//functions of the class
+	private:
+		float x,y,r; //x,y centre co-ords and radius.
+	//Accessible by member functions and any other functions
+	//provided the instance of the class is in scope
+	public:
+		Circle(float xx, float yy, float rr){
+		     r = (rr > 0.)? rr : 1.0f;
+		     x = xx;
+		     y = yy;
+		}	//Constructor
+		~Circle(){} //Destructor
+
+		//getter and setter of radius.
+		float GetR(){return r;} //Return radius
+		void SetR(float rr){
+		     if(rr > 0.)
+			     r = rr;
+		     else 
+			     r = 1.0f;
+		}	//Set radius
+
+}; 
+
 int main(int argc, char *argv[]){
      
      //Instantiation of a circle object
-	Circle* first = new Circle(10.0f, 15.0f, 5.0f);
-    // cout << "Radius of Object is " << first.GetR() << endl;
+	Circle first(10.0f, 5.0f, 15.0f);
+        cout << "Radius of Object is " << first.GetR() << endl;
 
      //This sets the Radius of object first to 10.0f.
-  //   first.SetR(10.0f);
+        first.SetR(10.0f);
      //This outputs the radius of object first using the getter function.
-    // cout << "Radius of Object is " << first.GetR() << endl;
+        cout << "Radius of Object is " << first.GetR() << endl;
 
      return 0;
 }
