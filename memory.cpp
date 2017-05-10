f6070aecf069da75b5dba30bc4763450e5937140
StaceIv <stacey.ivanovic007@gmail.com>
2017-05-09 18:49:43 -0700

    Memory

---
+++ memory.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+     
+ 	int *i; //pointer to an int.
+	i = new int; //new int object.
+
+	//regular pointer operations allowed
+	*i = 0;
+	i++;
+
+	cout << "address of i: " << &i << "\n";
+	delete i; //free memory where i was stored.
+
+	//dynamic arrays
+	char *buf; //pointer to a char
+ 	buf = new char[1024]; //size of the new character?
+
+	cout << "address buf: " << &buf << "\n";
+	cout << "size: " << sizeof(buf) << "\n";
+
+	delete [] buf; //free memory
+	
+	return 0;
+}
